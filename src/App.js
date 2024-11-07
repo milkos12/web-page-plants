18f5617eb10ed9032ecef8f69797751aabe6b496
Miller Ostos <millerandresoc@gmail.com>
2024-11-07 18:20:47 -0500

    feat About: add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import About from './components/about/about';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <About />
     </div>
   );
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,16 +1,32 @@
 import stylesImg from '../../scss/components/_images.module.scss';
 import stylesTitle from '../../scss/components/_titles.module.scss';
+import stylesShape from '../../scss/components/_shapes.module.scss';
+import stylesConteiner from '../../scss/components/_conteiners.module.scss';
+import stylesOther from '../../scss/components/_others.module.scss';
 
-export const About  = () => {
-    return(
-    <section>
-        <div>
+export const About = () => {
+    return (
+        <section>
+
             <div className={stylesImg.imgAbout}>
                 <h1 className={stylesTitle.titleAbout}>COLOMBIA VERDE</h1>
             </div>
-        </div>
-    </section>
+            <div className={stylesConteiner.conteinerAboutMain}>
+                <div className={stylesConteiner.conteinerAboutOne}>
+                    <span className={stylesShape.cicleAbout}></span>
+                    <span className={stylesTitle.titleAboutOne}>Colombia, Cundinamarca</span>
+                </div>
+                <div className={stylesConteiner.conteinerAboutTwo}>
+                    <span className={stylesTitle.titleAboutTwoWelcome}>Welcome to Colombia Verde, our passion is quality!</span>
+                </div>
+                <div>
+                    <p className={stylesOther.aobutParagraph}>
+                        Our history began with a clear and defined purpose, “To make a difference” and thanks to this purpose and our dedication, innovation and commitment to excellence, we have evolved and grown to become a benchmark in our region and our industry.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
-} 
+}
 
 export default About;
